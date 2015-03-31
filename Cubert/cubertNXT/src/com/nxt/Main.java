6824d46199ce64bd538e48cf3b588d6746dabd5f
vanessa_werner <vw017@hdm-stuttgart.de>
2015-03-31 15:50:49 +0200

    - added files to .gitignore
    -implemented communication from PC to NXT
    -tbd: bi-directional communication

--- Cubert/cubertNXT/src/com/nxt/Main.java
+++ Cubert/cubertNXT/src/com/nxt/Main.java
@@ -1,5 +1,10 @@
 package com.nxt;
 
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
 import lejos.nxt.Button;
 import lejos.nxt.LCD;
 import lejos.nxt.comm.NXTConnection;
@@ -9,20 +14,65 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		connect();
-	}
-
-	private static void connect() {
-
+		// set up variabel which temporarily stores input from pc
+		int recievedInt = 0;
+		char recievedString = 0;
+		
 		LCD.drawString("Right BT->USB Verbindung", 0, 0);
 		NXTConnection connection = null;
-		if(Button.waitForAnyPress() == Button.ID_RIGHT){
+		
+		if (Button.waitForAnyPress() == Button.ID_RIGHT) 
+		{
 			LCD.drawString("Waiting for USB connection", 0, 1);
 			connection = USB.waitForConnection();
 		}
 		LCD.clear();
 		LCD.drawString("connected", 0, 0);
+		
+		// open Streams for sending/recieving data
+		DataInputStream dis = connection.openDataInputStream();
+		DataOutputStream dos = connection.openDataOutputStream();
+		
 		Button.waitForAnyPress();
-	}
+		LCD.clear();
+		LCD.drawString("trying to recieve data...", 0, 0);
 
+		// stop programm by pressing left button
+		while (!Button.LEFT.isDown()) 
+		{
+
+			// send data to pc
+			try 
+			{
+				dos.write(56789);
+				dos.flush();
+			} catch (IOException e1) 
+			{
+				e1.printStackTrace();
+			}
+			// check whether data is available to read
+			if (connection.available() > 0) 
+			{
+				try 
+				{
+					LCD.clear();
+					recievedInt = dis.readInt();
+					LCD.drawInt(recievedInt, 0, 0);
+
+					// recieve keyboard input ;-)
+					/*
+					  recievedString = dis.readChar(); LCD.clear();
+					  LCD.drawChar(recievedString, 1, 1); LCD.refresh();
+					 */
+
+				} catch (IOException e) 
+				{
+					// close connection when error
+					connection.close();
+					e.printStackTrace();
+					System.exit(0);
+				}
+			}
+		}
+	}
 }
--- Cubert/cubertPC/src/pc/Main.java
+++ Cubert/cubertPC/src/pc/Main.java
@@ -1,31 +1,62 @@
 package pc;
 
-import lejos.nxt.Motor;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
 import lejos.pc.comm.NXTComm;
 import lejos.pc.comm.NXTCommException;
 import lejos.pc.comm.NXTCommFactory;
 import lejos.pc.comm.NXTInfo;
 
 public class Main {
-	
-public static void main(String[]args){
-	
-	connect();
-}
 
-//connect to NXT via USB
-private static void connect() {
-	NXTComm nxtComm; 
-	NXTInfo[] nxtInfo = null;
-	try {
-		System.out.println("Trying to connect...");
-		nxtComm = NXTCommFactory.createNXTComm(NXTCommFactory.USB);
-		nxtInfo =nxtComm.search(null);
-		nxtComm.open(nxtInfo[0]);
-	} catch (NXTCommException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
+	public static void main(String[] args) {
+
+		int numberToSend = 1234;
+		int recievedInt = 0;
+		NXTComm nxtComm;
+		NXTInfo[] nxtInfo = null;
+
+		try {
+			System.out.println("Trying to connect...");
+			nxtComm = NXTCommFactory.createNXTComm(NXTCommFactory.USB);
+			nxtInfo = nxtComm.search(null);
+			nxtComm.open(nxtInfo[0]);
+			System.out.println("Connected to " + nxtInfo[0].name);
+
+			// Get Streams for sending/recieving data
+			DataOutputStream dos = new DataOutputStream(nxtComm.getOutputStream());
+			DataInputStream dis = new DataInputStream(nxtComm.getInputStream());
+			try 
+			{
+				dos.writeInt(numberToSend);
+				
+				// send data through stream
+				dos.flush();
+				
+				//checking for available data to read
+					if (nxtComm.available() > 0) 
+					{
+						recievedInt = dis.readInt();
+						System.out.println("" + recievedInt);
+					}
+
+				// type on keyboard an send data char by char to NXT ;-)
+				/*
+				 * // BufferedReader reader = new BufferedReader(new
+				 * InputStreamReader(System.in)); String writeToNXT = null;
+				 * while(true){ writeToNXT = reader.readLine();
+				 * dos.writeChars(writeToNXT); dos.flush(); }
+				 */
+			} catch (IOException e)
+			{
+				System.out.println("can't communicate");
+				e.printStackTrace();
+			}
+
+		} catch (NXTCommException e) 
+		{
+			e.printStackTrace();
+		}
 	}
-	System.out.println("Connected to " + nxtInfo[0].name);
-}
 }
