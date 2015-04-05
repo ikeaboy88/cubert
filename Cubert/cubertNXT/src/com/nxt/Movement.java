87dc63a48120eb2c6bab98f7a3f7c462779d458e
Vanessa Werner <vw017@hdm-stuttgart.de>
2015-04-05 16:56:25 +0200

    move above middle- and sidecubie, remove sensor from cube

--- Cubert/cubertNXT/src/com/nxt/Movement.java
+++ Cubert/cubertNXT/src/com/nxt/Movement.java
@@ -4,6 +4,7 @@ import lejos.nxt.Button;
 import lejos.nxt.LCD;
 import lejos.nxt.Motor;
 import lejos.nxt.MotorPort;
+import lejos.nxt.NXTMotor;
 import lejos.util.Delay;
 
 /**
@@ -13,8 +14,10 @@ import lejos.util.Delay;
  *
  */
 public class Movement {
-	Boolean is_scanning = false;
+//	Automaton automation = new Automaton();
 	int tacho_count_current = 0;
+	
+	NXTMotor motor_b = new NXTMotor(MotorPort.B);
 
 	/**
 	 * Rotate the table for a given angle in degrees
@@ -35,77 +38,40 @@ public class Movement {
 		return angle_translated;
 	}
 
-	// rotate forward to remove sensor from cube
-
-	public void removeSensorFromCube() {
-		MotorPort.B.resetTachoCount();
-		tacho_count_current = -1;
-		LCD.drawString("removing", 0, 0);
-//		MotorPort.B.controlMotor(70, 2);
-		Motor.B.backward();
-		while (true) {
-//			Delay.msDelay(700);
-			if ( Math.abs(Motor.B.getTachoCount()) > tacho_count_current) {
-				// Cache Tachocount
-				tacho_count_current = Math.abs(Motor.B.getTachoCount());
-				LCD.drawInt(Motor.B.getTachoCount(), 0, 3);
-
-			} else {
-				Motor.B.stop();
-				Motor.B.resetTachoCount();
-				LCD.drawString("finished", 0, 1);
-				break;
-			}
+	//represents the colorsensors initial (start/stop) position
+	public void moveToInitial() {
+		motor_b.resetTachoCount();
+		motor_b.setPower(50);
+		while(Math.abs(motor_b.getTachoCount())<90)
+		{
+			motor_b.forward();
 		}
-		LCD.drawString("totally finished", 0, 2);
+		motor_b.stop();
+//		automation.setState(State.REMOVED);
+		
 	}
 
-	// When motor is in initial position move towards cube and start scanning
-	// rotate backwards to scan cube
-	// return value should be int for tracking purpose
-	public void scanMiddleCubi() {
-		MotorPort.B.resetTachoCount();
-		tacho_count_current = -1;
-			LCD.drawString("scanningMID!", 0, 0);
-			MotorPort.B.controlMotor(70, 1);
-
-			
-			while (Motor.B.isMoving()) {
-				if (Motor.B.getTachoCount() > tacho_count_current) {
-					// Cache Tachocount
-					tacho_count_current = Motor.B.getTachoCount();
-					LCD.drawInt(MotorPort.B.getTachoCount(), 0, 3);
-				//	Delay.msDelay(300);
-				} else {
-					MotorPort.B.controlMotor(60, 3);
-					Motor.B.resetTachoCount();
-					LCD.drawString("finished", 0, 1);
-					break;
-				}
-			}
-			LCD.drawString("totally finished", 0, 2);
+	//moves 180° forward = above middle cubie
+	public void moveAboveMiddleCubie() {
+		motor_b.resetTachoCount();
+		motor_b.setPower(50);
+		while(Math.abs(motor_b.getTachoCount())<180)
+		{
+			motor_b.forward();
+		}
+		motor_b.stop();
+//		automation.setState(State.MIDDLE);
 	}
 	
-	public void scanSideCubi() {
-		MotorPort.B.resetTachoCount();
-		tacho_count_current = -1;
-		LCD.drawString("SIDscann", 0, 0);
-		MotorPort.B.controlMotor(70, 2);
-		
-		while (Motor.B.isMoving()) {
-			if ( MotorPort.B.getTachoCount() > tacho_count_current) {
-				// Cache Tachocount
-				tacho_count_current = MotorPort.B.getTachoCount();
-				LCD.drawInt(MotorPort.B.getTachoCount(), 0, 3);
-				//Button.waitForAnyPress();
-				//Delay.msDelay(1000);
-			} else {
-				MotorPort.B.controlMotor(60, 3);
-				MotorPort.B.resetTachoCount();
-				LCD.drawString("finished", 0, 1);
-				break;
-			}
+	//moves 180° forward = above side cubie
+	public void moveAboveSideCubie() {
+		motor_b.resetTachoCount();
+		motor_b.setPower(50);
+		while(Math.abs(motor_b.getTachoCount())<270)
+		{
+			motor_b.backward();
 		}
-		LCD.drawString("totally finished", 0, 2);
+		motor_b.stop();
+//		automation.setState(State.SIDE);
 	}
 }
