0bf7e29278ca861f7587f7eff1ee0f976aa9a3ba
Vanessa Werner <vw017@hdm-stuttgart.de>
2015-04-06 00:02:46 +0200

    used rotateTo instead of motorcontrol

--- Cubert/cubertNXT/src/com/nxt/Movement.java
+++ Cubert/cubertNXT/src/com/nxt/Movement.java
@@ -1,6 +1,7 @@
 package com.nxt;
 
 import lejos.nxt.LCD;
+import lejos.nxt.Motor;
 import lejos.nxt.MotorPort;
 import lejos.nxt.NXTMotor;
 import lejos.nxt.NXTRegulatedMotor;
@@ -10,29 +11,32 @@ import lejos.util.Delay;
  * Perform movement actions like turn, tilt, hold, ... the cube
  */
 public class Movement {
-	//dummy state
+	// dummy state
+	// 1=sensor is removed
+	// 2=sensor is centered
+	// 2=sensor is edged
 	int state;
-	
+
 	// Regulated motor for the table
 	private NXTRegulatedMotor ma;
 	// Unregulated motor for the sensor
-	private NXTMotor mb;
+	private NXTRegulatedMotor mb;
 	// Unregulated motor for the arm
-	private NXTMotor mc; 
-	
-//	private Statemachine statemachine = new Statemachine();
+	private NXTMotor mc;
 
 	public Movement() {
+		state = 0;
 		ma = new NXTRegulatedMotor(MotorPort.A);
-		mb = new NXTMotor(MotorPort.B);
+		mb = new NXTRegulatedMotor(MotorPort.B);
 		mc = new NXTMotor(MotorPort.C);
-		initialPosition();
+		removeSensor();
 	}
 
 	/**
 	 * Rotate the table for a given angle in degrees
 	 * 
-	 * @param angle The angle the table should turn
+	 * @param angle
+	 *            The angle the table should turn
 	 * @return The angle the motor did actually turn
 	 */
 	public int rotateTable(int angle) {
@@ -46,7 +50,7 @@ public class Movement {
 
 		return angle_translated;
 	}
-	
+
 	public void tiltCube() {
 		mc.setPower(100);
 
@@ -70,11 +74,11 @@ public class Movement {
 			Delay.msDelay(200);
 		}
 	}
-	
+
 	public void releaseCube() {
 		mc.resetTachoCount();
 		mc.setPower(60);
-		
+
 		// move 110 degrees
 		while (Math.abs(mc.getTachoCount()) < 110) {
 			mc.forward();
@@ -85,98 +89,90 @@ public class Movement {
 	public void holdCube() {
 		mc.resetTachoCount();
 		mc.setPower(60);
-		
+
 		// move 110 degrees
 		while (Math.abs(mc.getTachoCount()) < 110) {
 			mc.backward();
 		}
 		mc.stop();
 	}
-	
-	//move cube to initial position with most distance to cube 
-	public void initialPosition(){
-		mb.resetTachoCount();
-		mb.setPower(70);
-		
-		
-		while (Math.abs(mb.getTachoCount()) <45 ) {
-			mb.forward();
-		}
-		mb.stop();
-		state = 0;
-	}
 
+	// Max degree = 200
+	// Start degree = 0
 	public void removeSensor() {
 		mb.resetTachoCount();
-		mb.setPower(70);
 
-		//when sensor is located in center
-		if(state == 2){
-			while (Math.abs(mb.getTachoCount()) <180 ) {
-				mb.backward();
-			}
+		// when sensor is centered
+		if (state == 2) {
+			mb.rotateTo(-200);
+			mb.stop();
+			System.out.println(mb.getTachoCount());
 		}
-		//when sensor is located on edge 
-		else if(state == 3){
-			while (Math.abs(mb.getTachoCount()) <120 ) {
-				mb.backward();
-			}
-		}else LCD.drawString("kein remove state", 0, 0);
-		
+		// when sensor is edged
+		else  if (state == 3) {
+			mb.rotateTo(-140);
+			mb.stop();
+			System.out.println(mb.getTachoCount());
 
-		mb.stop();
-		state=1;
-//		statemachine.setState(State.ColorsensorREMOVED);
+		}
+		// when sensor is initial
+		 else  if (state == 0) {
+			mb.rotateTo(30);
+			mb.stop();
+			System.out.println(mb.getTachoCount());
+		}
+
+		else{
+			LCD.drawString("kein remove state", 0, 0);
+		}
+
+		state = 1;
 	}
 
-	//moves 180 degrees forward = above middle cubie
 	public void moveSensorToCenter() {
 		mb.resetTachoCount();
-		mb.setPower(70);
 
-		//when sensor is removed 
-		if(state == 0){
-			
-			while (Math.abs(mb.getTachoCount()) < 180)	{
-				mb.forward();
-			}
-			
-		}else if( state == 1){
-			
-			while (Math.abs(mb.getTachoCount()) < 120)	{
-				mb.forward();
-			}
+		// when sensor is removed
+		if (state == 1) {
+			mb.rotateTo(200);
+			mb.stop();
+			System.out.println(mb.getTachoCount());
 		}
-		//when sensor is on edge 
-			else if(state == 3 ){
-			
-			while (Math.abs(mb.getTachoCount()) < 60)	{
-				mb.forward();
-			}
-		}else LCD.drawString("kein center state", 0, 1);
-		
-		mb.stop();
-		state=2;
+		// when sensor is on edge
+		else if (state == 3) {
+			mb.rotateTo(60);
+			mb.stop();
+			System.out.println(mb.getTachoCount());
+		} else{ 
+			LCD.drawString("kein center state", 0, 1);
+
+		}
+		state = 2;
 	}
-	
-	//moves 180 degrees forward = above side cubie
+
 	public void moveSensorToEdge() {
 		mb.resetTachoCount();
-		mb.setPower(70);
 
-		if(state == 0){
-			
-			while (Math.abs(mb.getTachoCount()) < 120) {
-				mb.forward();
-			}
-		}else if(state == 2){
-			
-			while (Math.abs(mb.getTachoCount()) < 60) {
-				mb.backward();
-			}
-		}else LCD.drawString("kein edge state", 0, 2);
+		// when sensor is removed
+		if (state == 1) {
+			mb.rotateTo(140);
+			mb.stop();
+			System.out.println(mb.getTachoCount());
+		}
+		// when sensor is centered
+		else if (state == 2) {
+			mb.rotateTo(-45);
+			mb.stop();
+			System.out.println(mb.getTachoCount());
+		} else{
+			LCD.drawString("kein edge state", 0, 2);
+
+		}
+		state = 3;
+	}
+	
+	public void clear() {
+		LCD.clear();
 		
-		mb.stop();
-		state=3;
 	}
 }
