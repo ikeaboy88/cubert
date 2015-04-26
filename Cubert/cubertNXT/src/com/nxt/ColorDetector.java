6cf00a09c763c01edd79c6c1125de9f5427b4f49
Vanessa Werner <vw017@hdm-stuttgart.de>
2015-04-26 13:40:12 +0200

    Implemented ColorDetector. Detection of red, yellow,green,white and blue as magenta.

---
+++ Cubert/cubertNXT/src/com/nxt/ColorDetector.java
@@ -0,0 +1,66 @@
+package com.nxt;
+
+import lejos.nxt.Button;
+import lejos.nxt.LCD;
+import lejos.nxt.SensorPort;
+import lejos.nxt.addon.ColorHTSensor;
+import lejos.robotics.Color;
+import lejos.util.Delay;
+
+/**
+ * For testing the HiTechnic color sensor (see lejos.nxt.addon.ColorHTSensor).
+ * @author BB
+ */
+public class ColorDetector {
+
+	ColorHTSensor cmps;
+	final static int INTERVAL = 200; // milliseconds
+
+	public ColorDetector(){
+		cmps = new ColorHTSensor(SensorPort.S3);
+	}
+	
+/*	public void calibrate(){
+		LCD.drawString("calibrate colorsensor", 0, 1);
+		LCD.drawString("white", 0, 2);
+		Button.waitForAnyPress();
+		LCD.clear();
+		// weiﬂ
+		cmps.initWhiteBalance();
+		//colorsensor.calibrateHigh();
+		LCD.drawString("black", 0, 3);
+		Button.waitForAnyPress();
+		cmps.initBlackLevel();
+		LCD.clear();
+		//colorsensor.calibrateLow();
+	}*/
+	
+	public void detectColor(){
+		String color = "Color";
+		String r = "R";
+		String g = "G";
+		String b = "B";
+		
+		String[] colorNames = {"Red", "Green", "Blue", "Yellow", "Magenta", "Orange",
+				             "White", "Black", "Pink", "Gray", "Light gray", "Dark Gray", "Cyan"			
+		};
+		
+		while(!Button.ESCAPE.isDown()) {
+			LCD.clear();
+			LCD.drawString(cmps.getVendorID(), 0, 0);
+			LCD.drawString(cmps.getProductID(), 0, 1);
+			LCD.drawString(cmps.getVersion(), 9, 1);
+			LCD.drawString(color, 0, 3);
+			LCD.drawInt(cmps.getColorID(),7,3);
+			LCD.drawString(colorNames[cmps.getColorID()], 0, 4);
+			LCD.drawString(r, 0, 5);
+			LCD.drawInt(cmps.getRGBComponent(Color.RED),1,5);
+			LCD.drawString(g, 5, 5);
+			LCD.drawInt(cmps.getRGBComponent(Color.GREEN),6,5);
+			LCD.drawString(b, 10, 5);
+			LCD.drawInt(cmps.getRGBComponent(Color.BLUE),11,5);
+			LCD.refresh();
+			Delay.msDelay(INTERVAL);
+		}
+	}
+}
