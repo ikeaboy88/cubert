018eb5b6357c9f09bf78e05036db7164a55d6938
Maximilian Braun <mb238@hdm-stuttgart.de>
2015-04-29 19:15:49 +0200

    Started with orderScanResult() and serializeOrderedScanResult(). Added some loops and checks to make the executeCompleteScan actually scan each side now (not tested yet)

--- Cubert/cubertNXT/src/com/nxt/Cube.java
+++ Cubert/cubertNXT/src/com/nxt/Cube.java
@@ -32,68 +32,106 @@ public class Cube {
 	public final char[] cubie_18_solved = {'W', 'x', 'x', 'x', 'G', 'x'}; // Edge:		White	, green
 	public final char[] cubie_19_solved = {'W', 'x', 'x', 'R', 'G', 'x'}; // Corner: 	White	, red	, green
 	
-	public final char[][] cube_solved = {	cubie_00_solved,
-											cubie_01_solved,
-											cubie_02_solved,
-											cubie_03_solved,
-											cubie_04_solved,
-											cubie_05_solved,
-											cubie_06_solved,
-											cubie_07_solved,
-											cubie_08_solved,
-											cubie_09_solved,
-											cubie_10_solved,
-											cubie_11_solved,
-											cubie_12_solved,
-											cubie_13_solved,
-											cubie_14_solved,
-											cubie_15_solved,
-											cubie_16_solved,
-											cubie_17_solved,
-											cubie_18_solved,
-											cubie_19_solved
+	public final char[][] cube_solved = {	cubie_00_solved, cubie_01_solved, cubie_02_solved, cubie_03_solved,
+											cubie_04_solved, cubie_05_solved, cubie_06_solved, cubie_07_solved,
+											cubie_08_solved, cubie_09_solved, cubie_10_solved, cubie_11_solved,
+											cubie_12_solved, cubie_13_solved, cubie_14_solved, cubie_15_solved,
+											cubie_16_solved, cubie_17_solved, cubie_18_solved,cubie_19_solved
 										};
+	
+	public char[] 	cubie_00, cubie_01, cubie_02, cubie_03, cubie_04, cubie_05 ,cubie_06, cubie_07, cubie_08,
+					cubie_09, cubie_10, cubie_11, cubie_12, cubie_13, cubie_14, cubie_15, cubie_16, cubie_17,
+					cubie_18, cubie_19;
+	
+	public char[][] cube_scrambled;
 	
 	public Cube() {
 		move = new Movement(true);
 		detect = new ColorDetector(SensorPort.S3, true);
 	}
 	
-	public void executeCompleteScan() {
+	//TODO:
+	public char[][] orderScanResult(char[] scan_result_vector) {
+		// overall faces count: 54
+		// center at 0, 9, 18, 27, 36, 45
+		// corner at 2, 4, 6, 8, 11, 13, 15, 17, 20, 22, 24, 26, 29, 31, 33, 35, 38, 40, 42, 44, 47, 49, 51, 53
+		// edge at (1+14),(10+23),(19+32),(28+41), ...? 37, 50, 46, 5, 3, 16, 12, 25, 21, 34, 30, 43, 39, 52, 7, 48
+		//TODO: Group all corners and edges into actual cubies
+		//TODO: Put cubies into the right cube index (according to the centers)
+		cube_scrambled[0][0] = 'b'; //actually put complete cubie signature into cube_scrambled array
+		cube_scrambled[0][1] = 'l';
+		cube_scrambled[0][2] = 'u';
+		cube_scrambled[0][3] = 'b';
+		//...
 		
-		// scan center
-		move.moveSensorToCenter();
-		Delay.msDelay(200);
-		detect.detectColor(200, 5);
+		return cube_scrambled;
+	}
+	
+	//TODO: Serialized representation of the scrambled cube that is used for transmission to PC
+	private char[] serializeOrderedScanResult(char[][] cube_scrambled) {
 		
-		//Scan edges
-		move.moveSensorToEdge();
-		Delay.msDelay(200);
-		detect.detectColor(200, 5);
-		//Button.waitForAnyPress();
-		for (int i = 0; i < 7; i++) {
+		return cube_scrambled[0];
+	}
+	
+	public char[] executeCompleteScan() {
+		char[] scan_result_vector = new char[0];
+		int index = 0;
+		
+		// Scan all 6 sides of the cube
+		for (int j = 0; j < 6; j++) {
+			
+			// scan center
+			move.moveSensorToCenter();
+			Delay.msDelay(200);
+			scan_result_vector[index] = detect.detectColor(200, 5);
+			index += 1;
+			
+			//Scan edge
+			move.moveSensorToEdge();
+			Delay.msDelay(200);
+			scan_result_vector[index] = detect.detectColor(200, 5);
+			index += 1;
+	
+			//Scan remaining 7 cubie-surfaces clockwise on the current upper side
+			for (int i = 0; i < 7; i++) {
+				Delay.msDelay(200);
+				move.rotateTable(45);
+				if (i % 2 == 1) {
+					move.moveSensorToEdge();
+				} else {
+					move.moveSensorToCorner();
+				}
+				scan_result_vector[index] = detect.detectColor(200, 5);
+				index += 1;
+			}
+			
+			//Prepare for tilt
 			Delay.msDelay(200);
 			move.rotateTable(45);
-			if (i % 2 == 1) {
-				move.moveSensorToEdge();
-			} else {
-				move.moveSensorToCorner();
+			//After 4 sides do an additional quarter turn
+			if (j == 4) {
+				move.rotateTable(90);
 			}
-			detect.detectColor(200, 5);
+			move.moveSensorToEdge();
+			Delay.msDelay(200);
+			move.removeSensor();
+			
+			//Tilt cube (2x after scanning the 5th side, not after scanning last side)
+			if (j < 5) {
+				for (int k = 0; k < 2; k++) {
+					move.holdCube();
+					Delay.msDelay(1000);
+					move.tiltCube();
+					Delay.msDelay(1000);
+					move.releaseCube();
+					Delay.msDelay(1000);
+					if (j != 4) {
+						break;
+					}
+				}
+			}
 		}
-		Delay.msDelay(200);
-		move.rotateTable(45);
-		move.moveSensorToEdge();
-		Delay.msDelay(200);
-		move.removeSensor();
-		
-		//Tilt cube
-		move.holdCube();
-		Delay.msDelay(1000);
-		move.tiltCube();
-		Delay.msDelay(1000);
-		move.releaseCube();
-		Delay.msDelay(1000);
+		return scan_result_vector;
 	}
 	
 }
