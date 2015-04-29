019ce9f20c1ac946448ea9be602a58d2edd62f1f
Maximilian Braun <mb238@hdm-stuttgart.de>
2015-04-29 11:13:08 +0200

    New class for the cube and its state. Put sequence for complete scan there

---
+++ Cubert/cubertNXT/src/com/nxt/Cube.java
@@ -0,0 +1,53 @@
+package com.nxt;
+
+import lejos.nxt.SensorPort;
+import lejos.util.Delay;
+
+public class Cube {
+
+	Movement move;
+	ColorDetector detect;
+	
+	public Cube() {
+		move = new Movement();
+		detect = new ColorDetector(SensorPort.S3);
+	}
+	
+	public void executeCompleteScan() {
+		
+		// scan center
+		move.moveSensorToCenter();
+		Delay.msDelay(200);
+		detect.detectColor(200, 5);
+		
+		//Scan edges
+		move.moveSensorToEdge();
+		Delay.msDelay(200);
+		detect.detectColor(200, 5);
+		//Button.waitForAnyPress();
+		for (int i = 0; i < 7; i++) {
+			Delay.msDelay(200);
+			move.rotateTable(45);
+			if (i % 2 == 1) {
+				move.moveSensorToEdge();
+			} else {
+				move.moveSensorToCorner();
+			}
+			detect.detectColor(200, 5);
+		}
+		Delay.msDelay(200);
+		move.rotateTable(45);
+		move.moveSensorToEdge();
+		Delay.msDelay(200);
+		move.removeSensor();
+		
+		//Tilt cube
+		move.holdCube();
+		Delay.msDelay(1000);
+		move.tiltCube();
+		Delay.msDelay(1000);
+		move.releaseCube();
+		Delay.msDelay(1000);
+	}
+	
+}
