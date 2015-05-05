0534228d7b425ca8232d0ab0e6f3f698f1130bbf
Maximilian Braun <mb238@hdm-stuttgart.de>
2015-05-05 15:16:29 +0200

    Output whether cubies are identical or different and whether cube is solved or scrambled

--- Cubert/cubertPC/src/com/pc/Main.java
+++ Cubert/cubertPC/src/com/pc/Main.java
@@ -1,5 +1,7 @@
 package com.pc;
 
+import java.util.Arrays;
+
 public class Main {
 
 	public static void main(String[] args) 
@@ -12,11 +14,11 @@ public class Main {
 		}
 		
 		int count = 0;
-		for (char[] cube_solved_vector: cube.cube_solved) {
+		for (char[] cube_solved_cubie: cube.cube_solved) {
 			System.out.println();
 			System.out.println();
 			System.out.println("Cubie : " + count + " solved: ");
-			for (char color: cube_solved_vector) {
+			for (char color: cube_solved_cubie) {
 				System.out.print(" " + color);
 			}
 			System.out.println();
@@ -24,7 +26,21 @@ public class Main {
 			for (char color: cube.cube_scrambled[count]) {
 				System.out.print(" " + color);
 			}
+
+			System.out.println();
+			if (Arrays.equals(cube.cube_scrambled[count], cube_solved_cubie)) {
+				System.out.println("IDENTICAL");
+			} else {
+				System.out.println("DIFFERENT");
+			}
 			count ++;
+		}
+		
+		System.out.println();
+		if (Arrays.deepEquals(cube.cube_solved, cube.cube_scrambled)) {
+			System.out.println("CUBE IS SOLVED");
+		} else {
+			System.out.println("CUBE IS SCRAMBLED");
 		}
 	}
 }
