9ad87b06b1e888d0878ae95eb185e92786f66bbc
Vanessa Werner <vw017@hdm-stuttgart.de>
2015-05-05 18:58:15 +0200

    Added Calibration.java to nxt and pc

---
+++ Cubert/cubertNXT/src/com/nxt/Calibration.java
@@ -0,0 +1,42 @@
+package com.nxt;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import lejos.nxt.Button;
+import lejos.nxt.LCD;
+
+public class Calibration {
+	
+	public static void main(String[] args) {
+		
+		Cube cube = new Cube();
+		
+		LCD.drawString("Press RIGHT button", 0, 0);
+		LCD.drawString("to start", 0, 1);
+		LCD.drawString("calibration", 0, 2);
+		if (Button.waitForAnyPress() == Button.ID_RIGHT) {
+			LCD.drawString("Calibrating", 0, 1);
+			cube.executeCompleteScan(true);
+		}
+		int[][] ref = cube.detect.rgb_ref;
+		List<Integer>rgb_values = new ArrayList<Integer>();
+		for(int [] reference_rgb : ref){
+			for (int i = 0; i <reference_rgb.length; i++)
+			{
+				rgb_values.add(reference_rgb[i]);
+			}
+		}
+		
+		String rgb_string = "";
+		for(int i = 0; i<rgb_values.size(); i++){
+			rgb_string += rgb_values.get(i);
+		}
+		
+		LCD.clear();
+		/* Connection between NXT and PC */
+		Connection connect_NXT = new Connection();
+		connect_NXT.connectToPC();
+		connect_NXT.sendRGBCalibration(rgb_string);
+	}
+}
---
+++ Cubert/cubertPC/src/com/pc/Calibration.java
@@ -0,0 +1,42 @@
+package com.pc;
+
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.ArrayList;
+
+public class Calibration {
+
+	public static void main(String[] args) {
+
+		Connection con = new Connection();
+		con.connectToNXT();
+
+		String reference_rgb = con.getReferenceRgbValues();
+		File calibration = new File("src/calibration/reference_RGB_values.txt");
+		if (!calibration.exists()) {
+
+			try {
+				calibration.createNewFile();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		//when file already exist, write in file 
+		try {
+			Writer file_writer = new FileWriter(calibration, true);
+//			for (int i = 0 ; i < reference_rgb.length ; i++ ){
+				file_writer.write(reference_rgb);
+				file_writer.close();
+//			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+}
