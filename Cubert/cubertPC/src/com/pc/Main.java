b39d220d66365f20f2c1a1b29325e0c929383abc
Maximilian Braun <mb238@hdm-stuttgart.de>
2015-05-05 14:44:58 +0200

    Show each cubie (solved & scrambled) beneath each other

--- Cubert/cubertPC/src/com/pc/Main.java
+++ Cubert/cubertPC/src/com/pc/Main.java
@@ -6,6 +6,25 @@ public class Main {
 	{
 		Connection connect_PC = new Connection();
 		connect_PC.connectToNXT();
-		connect_PC.getScanResultVector();
+		Cube cube = new Cube(connect_PC.getScanResultVector());
+		if (cube.cube_orientation == null) {
+			System.out.println("SCAN ERROR");
+		}
+		
+		int count = 0;
+		for (char[] cube_solved_vector: cube.cube_solved) {
+			System.out.println();
+			System.out.println();
+			System.out.println("Cubie : " + count + " solved: ");
+			for (char color: cube_solved_vector) {
+				System.out.print(" " + color);
+			}
+			System.out.println();
+			System.out.println("Cubie : " + count + " scrambled: ");
+			for (char color: cube.cube_scrambled[count]) {
+				System.out.print(" " + color);
+			}
+			count ++;
+		}
 	}
 }
