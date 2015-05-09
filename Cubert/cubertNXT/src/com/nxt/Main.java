616c60128855389adc80e42a8cf49e37653488b4
Vanessa Werner <vw017@hdm-stuttgart.de>
2015-05-09 16:55:43 +0200

    added option to set calibration mode or solving mode in main

--- Cubert/cubertNXT/src/com/nxt/Main.java
+++ Cubert/cubertNXT/src/com/nxt/Main.java
@@ -1,5 +1,9 @@
 package com.nxt;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 import lejos.nxt.Button;
 import lejos.nxt.LCD;
 
@@ -8,38 +12,73 @@ public class Main {
 	public static void main(String[] args) {
 		// Create object to execute movements on Cubert
 		Cube cube = new Cube();
-
-//		colorDetector.calibrate();
-		// ***********************************
-		LCD.drawString("Press button", 0, 0);
+		
+		/* Connection between NXT and PC */
+		Connection connect_NXT = new Connection();
+		connect_NXT.connectToPC();
+		
+		LCD.drawString("Press RIGHT button", 0, 0);
 		LCD.drawString("to start", 0, 1);
 		LCD.drawString("calibration", 0, 2);
-		Button.waitForAnyPress();
-		LCD.clear();
-		LCD.drawString("Calibration", 0, 0);
-		LCD.drawString("running", 0, 1);
-		cube.executeCompleteScan(true);
-		LCD.clear();
-		LCD.drawString("Press button", 0, 0);
-		LCD.drawString("to start", 0, 1);
-		LCD.drawString("recognition", 0, 2);
-		Button.waitForAnyPress();
-		LCD.clear();
-		
-		// "Runtime loop"
-		while (true) {
-			// Exit loop when escape button was pressed
-			if (Button.ESCAPE.isDown()) {
-				break;
+		if (Button.waitForAnyPress() == Button.ID_RIGHT) {
+			connect_NXT.sendMode(0);
+			LCD.drawString("Calibrating", 0, 1);
+			cube.executeCompleteScan(true);
+		int[][] ref = cube.detect.rgb_ref;
+		List<Integer>rgb_values = new ArrayList<Integer>();
+		for(int [] reference_rgb : ref){
+			for (int i = 0; i <reference_rgb.length; i++)
+			{
+				rgb_values.add(reference_rgb[i]);
 			}
-			// ...execute code below
-			
-			/* Connection between NXT and PC */
-			Connection connect_NXT = new Connection();
-			connect_NXT.connectToPC();
-			connect_NXT.sendScanResultVector(cube.executeCompleteScan());
-			Button.waitForAnyPress();
 		}
+		
+		String rgb_string = "";
+		for(int i = 0; i<rgb_values.size(); i++){
+			rgb_string += rgb_values.get(i)+",";
+		}
+		rgb_string = rgb_string.substring(0, rgb_string.length()-1);
+		
+		LCD.clear();
+		/* Connection between NXT and PC */
+		connect_NXT.sendRGBCalibration(rgb_string);
+		LCD.drawString("calibration done, press BT", 1, 1);
+		Button.waitForAnyPress();
+		LCD.clear();
+		}
+			
+			//get reference_rgb vector from file via dis
+			
+			// "Runtime loop"
+//			while (true) {
+				// Exit loop when escape button was pressed
+//				if (Button.ESCAPE.isDown()) {
+//					break;
+//				}
+				// ...execute code below
+				
+				connect_NXT.sendMode(1);
+				connect_NXT.sendScanResultVector(cube.executeCompleteScan());
+				Button.waitForAnyPress();
+//			}
+//		colorDetector.calibrate();
+		// ***********************************
+//		LCD.drawString("Press button", 0, 0);
+//		LCD.drawString("to start", 0, 1);
+//		LCD.drawString("calibration", 0, 2);
+//		Button.waitForAnyPress();
+//		LCD.clear();
+//		LCD.drawString("Calibration", 0, 0);
+//		LCD.drawString("running", 0, 1);
+//		cube.executeCompleteScan(true);
+//		LCD.clear();
+//		LCD.drawString("Press button", 0, 0);
+//		LCD.drawString("to start", 0, 1);
+//		LCD.drawString("recognition", 0, 2);
+//		Button.waitForAnyPress();
+//		LCD.clear();
+		
+
 		// ***********************************
 
 	}
--- Cubert/cubertPC/src/com/pc/Main.java
+++ Cubert/cubertPC/src/com/pc/Main.java
@@ -2,29 +2,37 @@ package com.pc;
 
 public class Main {
 
-	public static void main(String[] args) 
-	{
+	public static void main(String[] args) {
 		Connection connect_PC = new Connection();
 		connect_PC.connectToNXT();
+
+		int mode = connect_PC.getMode();
+		if (mode == 0) {
+			System.out.println("calibration mode");
+			Calibration cal = new Calibration(connect_PC);
+			cal.calibrate();
+			System.out.println("calibration done");
+		}
+		System.out.println("solving mode");
 		Cube cube = new Cube(connect_PC.getScanResultVector());
 		if (cube.cube_orientation == null) {
 			System.out.println("SCAN ERROR");
 		}
-		
+
 		int count = 0;
-		for (char[] cube_solved_vector: cube.cube_solved) {
+		for (char[] cube_solved_vector : cube.cube_solved) {
 			System.out.println();
 			System.out.println();
 			System.out.println("Cubie : " + count + " solved: ");
-			for (char color: cube_solved_vector) {
+			for (char color : cube_solved_vector) {
 				System.out.print(" " + color);
 			}
 			System.out.println();
 			System.out.println("Cubie : " + count + " scrambled: ");
-			for (char color: cube.cube_scrambled[count]) {
+			for (char color : cube.cube_scrambled[count]) {
 				System.out.print(" " + color);
 			}
-			count ++;
+			count++;
 		}
 	}
 }
