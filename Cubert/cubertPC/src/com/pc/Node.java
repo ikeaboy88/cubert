b5e1a66cdc86026ceeb8e6f849059f070a9eef10
Maximilian Braun <mb238@hdm-stuttgart.de>
2015-06-22 17:52:03 +0200

    Constructors added

--- Cubert/cubertPC/src/com/pc/Node.java
+++ Cubert/cubertPC/src/com/pc/Node.java
@@ -5,6 +5,9 @@ public class Node {
 	// Node we came from
 	private Node predecessor_node;
 	
+	// Hash value of the cube's current state
+	private int state_hash;
+	
 	// Cost since start
 	private int g_costs;
 	
@@ -14,6 +17,22 @@ public class Node {
 	// Action we performed at the predecessor node to get to this current node
 	private char action;
 
+	public Node(int state_hash) {
+		this.predecessor_node = null;
+		this.state_hash = state_hash;
+		this.g_costs = 0;
+		this.h_costs = 0.0;
+		this.action = '\u0000';
+	}
+	
+	public Node(Node predecessor_node, int state_hash, int g_costs, double h_costs, char action) {
+		this.predecessor_node = predecessor_node;
+		this.state_hash = state_hash;
+		this.g_costs = g_costs;
+		this.h_costs = h_costs;
+		this.action = action;
+	}
+	
 	public double getTotalCosts() {
 		
 		return g_costs + h_costs;
