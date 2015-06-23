3d3a2426153d10c15fb8a055aeb55c4c6d0fb4cc
Maximilian Braun <mb238@hdm-stuttgart.de>
2015-06-23 13:51:33 +0200

    Getter and setter for Node attributes

--- Cubert/cubertPC/src/com/pc/Node.java
+++ Cubert/cubertPC/src/com/pc/Node.java
@@ -17,6 +17,7 @@ public class Node {
 	// Action we performed at the predecessor node to get to this current node
 	private char action;
 
+	// Constructor for start node
 	public Node(int state_hash) {
 		this.predecessor_node = null;
 		this.state_hash = state_hash;
@@ -25,6 +26,7 @@ public class Node {
 		this.action = '\u0000';
 	}
 	
+	// Constructor for further nodes
 	public Node(Node predecessor_node, int state_hash, int g_costs, double h_costs, char action) {
 		this.predecessor_node = predecessor_node;
 		this.state_hash = state_hash;
@@ -36,5 +38,45 @@ public class Node {
 	public double getTotalCosts() {
 		
 		return g_costs + h_costs;
+	}
+
+	public Node getPredecessor_node() {
+		return predecessor_node;
+	}
+
+	public void setPredecessor_node(Node predecessor_node) {
+		this.predecessor_node = predecessor_node;
+	}
+
+	public int getState_hash() {
+		return state_hash;
+	}
+
+	public void setState_hash(int state_hash) {
+		this.state_hash = state_hash;
+	}
+
+	public int getG_costs() {
+		return g_costs;
+	}
+
+	public void setG_costs(int g_costs) {
+		this.g_costs = g_costs;
+	}
+
+	public double getH_costs() {
+		return h_costs;
+	}
+
+	public void setH_costs(double h_costs) {
+		this.h_costs = h_costs;
+	}
+
+	public char getAction() {
+		return action;
+	}
+
+	public void setAction(char action) {
+		this.action = action;
 	}
 }
